d38568318b65709c1445a55e180650764aaa0e1c
kammulchandani <kam.mulchandani@gmail.com>
2017-04-01 03:01:19 -0700

    Added weight dist, arrays and enum type constraints

--- constraints.sv
+++ constraints.sv
@@ -10,7 +10,13 @@ class constraints extends uvm_sequence_item;
   rand bit           dst_flag; 
   rand logic [4:0]   distribution;
  
-  // Declare an enum data-type. 
+  // constraint for array
+  rand int  f; 
+  int fib[5] = {0,4,6,7,8}; 
+  // enum data-type. 
+  typedef  enum {BYTE, WORD, LOAD} length_e; 
+  rand  length_e  len;
+  bit [31:0] w_byte=1, w_word=3, w_load=5;
  
   `uvm_object_utils_begin(constraints)
     `uvm_int_field(addr, UVM_ALL_ON)
@@ -23,6 +29,8 @@ class constraints extends uvm_sequence_item;
     `uvm_int_field(size , UVM_ALL_ON)
     `uvm_int_field(dst_flag , UVM_ALL_ON)
     `uvm_int_field(distribution , UVM_ALL_ON)
+    `uvm_field_array_int(fib , UVM_ALL_ON)
+    `uvm_field_enum(length_e, len, UVM_ALL_ON)
   `uvm_object_utils_end
 
    function new( string name="constraints");
@@ -32,16 +40,24 @@ class constraints extends uvm_sequence_item;
    // Values are in hexadecimal. 
    // Constraints execute in Consecutive style. 
    // Inputs to the DUT are randomized. 
+   
+   // inside construct 
    constraint   addr_c    { addr inside {[0:100]};}
-   constraint   notaddr_c { !(not_addr inside {[0:100]};)}
    constraint   data_c    { data inside {[0:100]};}
-   constraint   wr_rd_c   { wr !== rd ;}
    constraint   size_c    { size inside {[0,1,3,7,0f]};}
+
+   // Inverted Constraints 
+   constraint   notaddr_c { !(not_addr inside {[0:100]};)}
+   constraint   wr_rd_c   { wr !== rd ;}
    constraint   src_c     { src == 2**size; }
    // Conditional Contraint 
    constraint   dst_c     { if (dst_flag) {dst == src;} 
 			    else {dst !== src; } }
    constraint   dist_c    { distribution dist {[0:7]:=10, [8:21]:=90, [22:26]:/20, [27:31]:/30}; 
                             }
+   constraint   len_c     { len dist { BYTE:= w_byte, 
+                                       WORD:= w_word,
+                                       LOAD:= w_load };
+                           }
 
 endclass 

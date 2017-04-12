ca90d43ecda3c49cebb95d95bfcc3b9ed18972c5
kammulchandani <kam.mulchandani@gmail.com>
2017-04-11 23:54:43 -0700

    Initial version

---
+++ sample_5_9.sv
@@ -0,0 +1,21 @@
+module static_variable; 
+
+  class transaction; 
+  
+    static int count = 0;
+    int        id;
+    
+    function new();
+      id = count++; 
+    endfunction 
+  
+  endclass 
+  
+  transaction t1, t2; 
+  initial begin
+    t1=new();
+    t2=new();
+    $display(" Second id  %h, count %h ", t2.id, t2.count);
+  end 
+
+endmodule 
